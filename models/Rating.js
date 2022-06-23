c9013a3079cfcf356a386076487fc694129b4e2a
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-06-23 10:32:39 -0400

    created index.js for models. Also started on the Rating model

---
+++ models/Rating.js
@@ -0,0 +1,6 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Rating extends Model {}
+
+module.exports = Rating;
---
+++ models/index.js
@@ -0,0 +1,14 @@
+// example index.js
+
+const User = require('./User');
+const Rating = require('./Rating');
+
+User.hasMany(Rating, {
+  foreignKey: 'user_id',
+});
+
+Rating.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Rating };
