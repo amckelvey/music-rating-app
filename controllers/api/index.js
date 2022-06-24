5f4c2238cd97d2fae5b02ddcf43644297ba11664
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-06-24 15:45:36 -0400

    save started spotify routes file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const ratingRoutes = require('./ratingRoutes');
 const reviewRoutes = require('./reviewRoutes');
+const spotifyRoutes = require('./spotify');
 
 router.use('/users', userRoutes);
 router.use('/ratings/', ratingRoutes);
 router.use('/reviews/', reviewRoutes);
-
+router.use('/spotify/', spotifyRoutes);
 module.exports = router;
