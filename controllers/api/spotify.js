086a9409bb2a31db85ad6b084a2010a24fa174a5
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-06-25 14:44:06 -0400

    created a get artist info from spotify route

--- controllers/api/spotify.js
+++ controllers/api/spotify.js
@@ -25,31 +25,46 @@ async function spotifyAuthorize() {
   return spotifyApi;
 }
 
-// GET /api/spotify/:artist
+// GET /api/spotify/albums/:artist
 // Receives an artist name input by the user
 // does a GET /v1/search search query to spotify to get the artist_id
 // does a GET /v1/artists/{artist_id}/albums request to get album info
-// Returns the relevant album info
+// Returns an array of album info
 
-router.get('/:artist', async (req, res) => {
+router.get('/albums/:artist', async (req, res) => {
   try {
     const spotifyApi = await spotifyAuthorize();
-  
     const searchArtistData = await spotifyApi.searchArtists(req.params.artist, {limit: 1});
-  
     const artistId = searchArtistData.body.artists.items[0].id;
-
     const albumData = await spotifyApi.getArtistAlbums(artistId ,{include_groups: 'album', market: 'US'});
-
     const albumArray = albumData.body.items;
-
-
     res.status(200).json(albumArray);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+
+// GET /api/spotify/artist/:artist
+// Receives an artist name input by the user
+// does a GET /v1/search search query to spotify to get the artist_id
+// does a GET /v1/artists/{id} request to get album info
+router.get('/artist/:artist', async (req, res) => {
+  try {
+    const spotifyApi = await spotifyAuthorize();
+    const searchArtistData = await spotifyApi.searchArtists(req.params.artist, {limit: 1});
+    const artistId = searchArtistData.body.artists.items[0].id;
+    console.log(artistId);
+    const artistData = await spotifyApi.getArtist(artistId);
+
+    res.status(200).json(artistData.body);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+
 
 module.exports = router;
 
