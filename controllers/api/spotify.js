a0cd92764c8ebb4dd6cece1a10dedc91e7096dfb
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-06-25 14:15:45 -0400

    created a spotify route which queries the spotify API for an artist's album data

--- controllers/api/spotify.js
+++ controllers/api/spotify.js
@@ -1,8 +1,55 @@
 var SpotifyWebApi = require('spotify-web-api-node');
+const router = require('express').Router();
 
-// GET /api/rating/:artist
+// will likely need to move this
+async function spotifyAuthorize() {
+  var clientId = 'b8613864536c4491b058d23befbec540',
+  clientSecret = 'fadd7cafd3d840d7b4d14180fc84ebb4';
+  spotifyApi = new SpotifyWebApi({
+    clientId: clientId,
+    clientSecret: clientSecret
+  });
+
+  let data;
+  try {
+    data = await spotifyApi.clientCredentialsGrant();
+    console.log('The access token expires in ' + data.body['expires_in']);
+    console.log('The access token is ' + data.body['access_token']);
+
+    // Save the access token so that it's used in future calls
+    spotifyApi.setAccessToken(data.body['access_token']);
+  } catch (err) {
+    console.log('Something went wrong when retrieving an access token', err);
+  }
+  // how do a return the spotify object?
+  return spotifyApi;
+}
+
+// GET /api/spotify/:artist
 // Receives an artist name input by the user
 // does a GET /v1/search search query to spotify to get the artist_id
 // does a GET /v1/artists/{artist_id}/albums request to get album info
 // Returns the relevant album info
+
+router.get('/:artist', async (req, res) => {
+  try {
+    const spotifyApi = await spotifyAuthorize();
+  
+    const searchArtistData = await spotifyApi.searchArtists(req.params.artist, {limit: 1});
+  
+    const artistId = searchArtistData.body.artists.items[0].id;
+
+    const albumData = await spotifyApi.getArtistAlbums(artistId ,{include_groups: 'album', market: 'US'});
+
+    const albumArray = albumData.body.items;
+
+
+    res.status(200).json(albumArray);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
 
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: 'Super secret secret', // use the .env to store this
   cookie: {},
   resave: false,
   saveUninitialized: true,
