442dfddbf79e4231c7c99cdf1752b1870c71fb40
Tazaev <tonymurtazaev@gmail.com>
2022-06-27 11:13:13 -0400

    added limks to main.hdb

--- controllers/api/ratingRoutes.js
+++ controllers/api/ratingRoutes.js
@@ -2,23 +2,55 @@ const router = require('express').Router();
 const { User, Rating } = require('../../models');
 
 
-// GET /api/rating/:album_id
+// GET /api/rating/average/:album_id
 // Receives a spotify album id
-// returns the ratings scores of that album
+// returns the average score of that album
 // req.params.album_id
-router.get('/:album_id', async (req, res) => {
+router.get('/average/:album_id', async (req, res) => {
   try {
     console.log("Get Ratings for album");
-    console.log("Album id is: " + req.params.album_id);
-    const dbscoreData = await Rating.findAll({
+    const scoreData = await Rating.findAll({
       attributes: ['score'],
       where: {
         album_id: req.params.album_id
       }
     });
+    let scores = scoreData.map((score) => score.score);
+    let total = 0;
+    for(let i = 0; i < scores.length; i++) {
+      total += scores[i];
+    }
+    const rawAvg = total / scores.length;
+    const average = Math.round(rawAvg * 10) / 10
+    // also return number of votes
+    res.status(200).json(average);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-    // const scoreData = dbscoreData.get({ plain: true });
-    res.status(200).json(dbscoreData);
+// // GET /api/rating/:user_id/:album_id
+// Receives a spotify album id, and user_id through the req body
+// uses findOne
+// returns rating object for the one album, excluding the review
+// req.params.album_id
+router.get('/:user_id/:album_id', async (req, res) => {
+  try {
+    console.log("Get Ratings for album");
+    const ratingData = await Rating.findOne({
+      attributes: {exclude: ['review']},
+      where: {
+        album_id: req.params.album_id,
+        user_id: req.params.user_id
+      }
+    });
+    if (!ratingData) {
+      res.status(404).json({ message: 'No Rating with this id!'});
+      return;
+    }
+    const rating = ratingData.get({ plain: true });
+    res.status(200).json(rating);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -27,24 +59,46 @@ router.get('/:album_id', async (req, res) => {
 
 // POST /api/rating/
 // Receives rating info in the request
+// checks if the user is logged in
 // creates a new rating
+router.post('/', async (req, res) => {
+  // create a new category
+  try {
+    const ratingData = await Rating.create(req.body);
+    res.status(200).json(ratingData);
+  } catch (err) {
+      res.status(500).json(err);
+      console.log(err);
+  }
+});
+
 
 // PUT /api/rating/:id
 // Receives rating a rating id, will need to check the user_id and album_id
+// checks if the user is logged in
 // updates an existing rating
-
-//
-
-
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const ratingData = await Rating.update(req.body, {
+      attributes: {exclude: ['review']},
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!ratingData[0]) {
+      res.status(404).json({ message: 'No Rating with this id!'});
+      return;
+    }
+    res.status(200).json(ratingData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 
 
 module.exports = router;
 
-// this may need to be done in another file
+// this may need to be done in another file, using spotify routes
 
-// GET /api/rating/:artist
-// Receives an artist name input by the user
-// does a GET /v1/search search query to spotify to get the artist_id
-// does a GET /v1/artists/{artist_id}/albums request to get album info
-// Returns the relevant album info
--- models/index.js
+++ models/index.js
@@ -1,24 +1,12 @@
 const User = require('./User');
 const Rating = require('./Rating');
-const Review = require('./Review');
 
 User.hasMany(Rating, {
-  foreignKey: 'user_id',
+  foreignKey: 'user_id'
 });
 
-Rating.belongsTo(User);
-
-User.hasMany(Review, {
-  foreignKey: 'user_id',
+Rating.belongsTo(User, {
+  foreignKey: 'user_id'
 });
 
-Rating.hasOne(Review);
-
-Review.belongsTo(Rating, {
-  foreignKey: 'rating_id'
-});
-
-
-
-
-module.exports = { User, Rating, Review };
+module.exports = { User, Rating };
